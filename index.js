71b0ed7dbc150cf069be3114dee264b74a40e67a
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-11 16:48:14 +0500

    terrible varibale idea will be removing it now

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-import { tweetsData } from "./data.js";
-const mInput = document.getElementById('tweet-input')
-const mBtn = document.getElementById('tweet-btn')
-const feed = document.getElementById('feed')
-mBtn.addEventListener('click', ()=>{
- const contents = mInput.value
-    console.log(contents);
-    
-})
-function getFeedHtml(){
-
-    let feedHtml=''
-    tweetsData.forEach(tweet => {
-        const profilePic = tweet.profilePic
-        const tweetHandle = tweet.handle
-        const text = tweet.tweetText
-        const replies = tweet.replies
-        const likes = tweet.likes
-        const retweets = tweet.retweets
-        feedHtml = `<div class="tweet">
+import { tweetsData } from './data.js';
+const mInput = document.getElementById('tweet-input');
+const mBtn = document.getElementById('tweet-btn');
+const feed = document.getElementById('feed');
+mBtn.addEventListener('click', () => {
+	const contents = mInput.value;
+	console.log(contents);
+});
+function getFeedHtml() {
+	let feedHtml = '';
+    let profilePic
+    let tweetHandle 
+    let text 
+    let replies
+    let likes 
+    let retweets 
+	tweetsData.forEach((tweet) => {
+		 profilePic = tweet.profilePic;
+		 tweetHandle = tweet.handle;
+		 text = tweet.tweetText;
+		 replies = tweet.replies;
+		 likes = tweet.likes;
+		 retweets = tweet.retweets;
+		feedHtml += `<div class="tweet">
         <div class="tweet-inner">
             <img src="${profilePic}" class="profile-pic">
             <div>
@@ -25,32 +29,35 @@ function getFeedHtml(){
                 <p class="tweet-text">${text}</p>
                 <div class="tweet-details">
                     <span class="tweet-detail">
+                    <i class='fa-regular fa-comment-dots'/>
                     ${replies}
                     </span>
                     <span class="tweet-detail">
+                    <i class='fa-solid fa-heart'/>
+
                     ${likes}
                     </span>
                     <span class="tweet-detail">
+                    <i class='fa-regular fa-retweet'/>
+
                      ${retweets}
                     </span>
                 </div>   
             </div>            
         </div>
     </div>
-    `
-    });
-return feedHtml
-
+    `;
+	});
+	return feedHtml;
 }
 
-function render(){
-  /*   Challenge:
+function render() {
+	/*   Challenge:
 1. Take control of the ‘feed’ div.
 2. Render the HTML returned by the getFeedHtml 
    function to the 'feed' div. 
    See if you can do this with just one line of code!
 */
-    feed.innerHTML=getFeedHtml()
-
+	feed.innerHTML = getFeedHtml();
 }
-render()
+render();
